2ed772c48acf48ebcd4ffb57fce85f71cefb9761
pivja <nithiphonbuu@hotmail.com>
2018-02-03 18:05:25 +0700

    test commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Admin_Main()
+        {
+            ViewBag.Message = "Your Admin page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void BindData()
+        {
+            MySqlConnection con = new MySqlConnection("server = 119.59.122.157; user id = dev; persistsecurityinfo = True; database = tms;password=LHITs12345678");
+            con.Open();
+
+            MySqlCommand cmd = new MySqlCommand("select * from destination_mas", con);
+        }
+    }
+}
